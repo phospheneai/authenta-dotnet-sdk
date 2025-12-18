ca8c561ce15d40421d396ec948b94cb9d616cfc4
dhamotharan <dhamotharan@LAPTOP-RR3SMT5Q>
2025-12-19 04:39:13 +0530

    updated source image path in sample project.

--- Authenta.SDK.TestClient/Program.cs
+++ Authenta.SDK.TestClient/Program.cs
@@ -8,21 +8,21 @@ class Program
     {
         Console.WriteLine("Authenta SDK Test Client");
 
-        var ex1 = @"C:\DG-client-project\authenta-python-sdk\data_samples\nano_img.png";
-
-
-        var client = new __AuthentaClient(new AuthentaOptions
+        var baseDir = AppContext.BaseDirectory;
+        var imagePath = Path.GetFullPath(Path.Combine(baseDir,  "../../../..", "data_samples", "nano_img.png"));
+        Console.WriteLine("source data path: " + imagePath);
+        var options = new AuthentaOptions
         {
             BaseUrl = Environment.GetEnvironmentVariable("AUTHENTA_BASE_URL"),
             ClientId = Environment.GetEnvironmentVariable("AUTHENTA_CLIENT_ID"),
             ClientSecret = Environment.GetEnvironmentVariable("AUTHENTA_CLIENT_SECRET")
-        });
-
+        };
+        
         if (string.IsNullOrEmpty(options.ClientId) || string.IsNullOrEmpty(options.ClientSecret)){
             throw new InvalidOperationException("Authenta credentials are not configured.");
         }
-
-        var result = await client.UploadFileAsync(ex1, "AC-1");
+        var client = new AuthentaClient(options);
+        var result = await client.UploadFileAsync(imagePath, "AC-1");
 
         Console.WriteLine($"Media ID: {result.Mid}");
     }
